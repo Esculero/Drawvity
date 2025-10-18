4e6b1a2000c5cce1833c9f3b1c64c0d0ba4ce3ff
MariaS210 <maria.sumedre@gmail.com>
2025-10-18 11:24:45 +0300

    Add an enemy
    Make it activate/deactivate when pressing E

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -5,6 +5,7 @@ public class Robot : MonoBehaviour
 {
     private Rigidbody2D body;
     private bool shouldMove = false; // When true the robot moves right at 'speed'
+    private bool hasEnemy = true;
 
     [SerializeField] private float speed = 5f;
 
@@ -13,11 +14,11 @@ public class Robot : MonoBehaviour
         body = GetComponent<Rigidbody2D>();
     }
 
-    //// Start is called once before the first execution of Update after the MonoBehaviour is created
-    //void Start()
-    //{
-
-    //}
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log($"Enemy is real: {hasEnemy}");
+    }
 
     // Update is called once per frame
     void Update()
@@ -25,6 +26,11 @@ public class Robot : MonoBehaviour
         if (Keyboard.current != null && Keyboard.current.spaceKey.wasPressedThisFrame)
         {
             shouldMove = !shouldMove;
+        }
+        if (Keyboard.current != null && Keyboard.current.eKey.wasPressedThisFrame)
+        {
+            hasEnemy = !hasEnemy;
+            Debug.Log($"Enemy is real: {hasEnemy}");
         }
         body.linearVelocity = new Vector2(shouldMove ? speed : 0f, body.linearVelocity.y);
     }
@@ -37,13 +43,27 @@ public class Robot : MonoBehaviour
         if (collision.contactCount > 0)
             contactPoint = collision.GetContact(0).point;
 
-        if (other.name == "LevelExit")
+        if (other.tag == "Finish")
         {
             Debug.Log("Robot reached the level exit!");
             shouldMove = false;
             body.linearVelocity = Vector2.zero;
             MoveToNextLevel(other);
-            return;
+        }
+
+        if (other.tag == "Enemy")
+        {
+            if (hasEnemy)
+            {
+                Debug.Log("Robot hit an enemy! Stopping movement.");
+                Destroy(this.gameObject); // ???
+            }
+            else
+            {
+                Destroy(other.GetComponent<Rigidbody2D>());
+                Destroy(other.GetComponent<Collider2D>());
+                Debug.Log("Robot passed through the enemy safely.");
+            }
         }
 
         Debug.Log($"Robot collided with '{other.name}' at {contactPoint} - relativeVelocity={collision.relativeVelocity}");
@@ -52,6 +72,7 @@ public class Robot : MonoBehaviour
     void MoveToNextLevel(GameObject other)
     {
         Destroy(other);
+        Destroy(this.gameObject);
         Debug.Log("Transitioning to the next level...");
     }
 }
