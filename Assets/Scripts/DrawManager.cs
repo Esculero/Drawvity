0ba61bc0a1a233ad3c321d028c817ef53c1f59d1
Berechet Lucian <lucian.ion45@yahoo.com>
2025-10-18 13:54:35 +0300

    Drawing is limited by the amount of ink left

--- Assets/Scripts/DrawManager.cs
+++ Assets/Scripts/DrawManager.cs
@@ -6,26 +6,32 @@ public class DrawManager : MonoBehaviour
 {
     #region Input hook
     // TODO - hook to InputManager when Andrei fucking does it, lazy ass piece of work
-    private InputSystem_Actions inputSystem = new();
+    private InputSystem_Actions inputSystem;
     private Vector2 drawPos;
     #endregion
 
     #region Ink control
+    
     [SerializeField]
-    private double[] InkPercentage = { 100f, 100f, 100f };
-
+    private double[] InkPercentage = {100.0f, 100.0f, 100.0f };
     private int SelectedInk = 0;
+    
+    [SerializeField]
+    private double PaintingCost = 1000.0f;
     #endregion
 
     #region Line creation
     private LineRenderer[] lines;
+    MeshCollider newMeshCollider;
 
     private LineRenderer currentLine;
     private Vector3 PreviousPosition;
 
     [SerializeField]
     private float MinDistance = 0.1f;
-    [SerializeField,Range(0.1f,0.2f)] private float Width = 0.1f;
+
+    [SerializeField, Range(0.1f, 0.2f)] 
+    private float Width = 0.2f;
     
 
     #endregion
@@ -45,13 +51,12 @@ public class DrawManager : MonoBehaviour
         {
             CreateLine();
         };
+
         inputSystem.Player.IsDrawing.canceled += Context =>
         {
             FinishDrawingLine();
         };
 
-        // pe inputSystem.Player.IsDrawing.started trebuie gestionata desenarea unui nou obiect
-        // pe inputSystem.Player.IsDrawing.canceled trebuie gestionata finalizarea desenarii obiectului
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -65,38 +70,46 @@ public class DrawManager : MonoBehaviour
     {
         if(currentLine != null)
         {
+            Debug.Log(InkPercentage[1]);
             Vector3 CurrentPosition = Camera.main.ScreenToWorldPoint(drawPos);
             CurrentPosition.z = 0f;
 
-
-            if (Vector3.Distance(CurrentPosition, PreviousPosition) > MinDistance)
+            float distance =  Vector3.Distance(CurrentPosition, PreviousPosition);
+            if (distance > MinDistance && InkPercentage[1] > 0)
             {
                 currentLine.positionCount++;
                 currentLine.SetPosition(currentLine.positionCount - 1, CurrentPosition);
                 PreviousPosition = CurrentPosition;
+                InkPercentage[1] -= PaintingCost * distance * Time.deltaTime;
+                
             }
-
-            
         }
     }
 
     void CreateLine()
     {
-        GameObject newLine = new GameObject();
-        currentLine = newLine.AddComponent<LineRenderer>();
-
         Vector3 CurrentPosition = Camera.main.ScreenToWorldPoint(drawPos);
         CurrentPosition.z = 0f;
+        PreviousPosition = CurrentPosition;
+
+        GameObject newLine = new GameObject();
+        currentLine = newLine.AddComponent<LineRenderer>();
+        // newMeshCollider = newLine.AddComponent<MeshCollider>();
+
+        // Mesh mesh = new Mesh();
+        // currentLine.BakeMesh(mesh, true);
+        // newMeshCollider.sharedMesh = mesh;
 
         currentLine.positionCount = 1;
         currentLine.startWidth = Width;
         currentLine.endWidth = Width;
-            
+
+
         currentLine.SetPosition(0, CurrentPosition);
 
         lines.Append(currentLine);
 
-        GameObject.Instantiate(newLine, this.transform);
+        currentLine.transform.SetParent(this.transform);
     }
     
     void FinishDrawingLine()
